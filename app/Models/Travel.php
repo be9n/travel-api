950b3caa848c49efd8606f293e24e2baf95cacfd
be9n <bed.cool48@gmail.com>
2023-10-28 17:43:20 +0300

    DB structure && Travels public api

---
+++ app/Models/Travel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+// use Cviebrock\EloquentSluggable\Sluggable;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Travel extends Model
+{
+    use HasFactory, HasSlug, HasUuids;
+
+    protected $table = 'travels';
+
+    protected $fillable = [
+        'is_public',
+        // 'slug',
+        'name',
+        'description',
+        'number_of_days',
+    ];
+
+    public function tours() {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function numberOfNights(): Attribute{
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+
+    // public function getNumberOfNightsAttribute(){
+    //     return $this->number_of_days - 1;
+    // }
+}
